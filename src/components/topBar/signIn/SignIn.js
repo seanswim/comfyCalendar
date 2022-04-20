7e60ef6740b0e6802e3b8cb8a7ee0a3655f63fb7
seanswim <swim1720@gmail.com>
2022-04-20 10:47:19 +0900

    CREATE: firebase setup

--- src/components/topBar/signIn/SignIn.js
+++ src/components/topBar/signIn/SignIn.js
@@ -1,8 +1,15 @@
 import { Button, IdContainer, Input, NormalSignInContainer, PasswordContainer, SignInBackground, SignInContainer } from "../../../styles/topBarStyles/signInStyles/SignInStyles";
 import { useState, useRef, useEffect } from "react"; 
 import { FaUserCircle, FaKey } from "react-icons/fa";
+import sha256 from 'crypto-js/sha256';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const SignIn = ({ openSginInModal }) => {
+
+  const passwordHash = sha256('1q2w3e4r').toString();
+
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('')
 
   //Outside click detecting
   const ref = useRef();
@@ -20,19 +27,28 @@ const SignIn = ({ openSginInModal }) => {
     };
   }, [])
 
+  //Sign in with Email and password
+  const handleSignin = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, )
+  }
+
+  const handleIdChange = (e) => setId(e.target.value)
+  const handlePWChange = (e) => setPassword(e.target.value)
+
   return (
     <SignInBackground>
       <SignInContainer ref={ref}>
         <NormalSignInContainer>
           <IdContainer>
             <FaUserCircle />
-            <Input />
+            <Input value={id} onChange={handleIdChange}/>
           </IdContainer>
           <PasswordContainer>
             <FaKey />
-            <Input type='password'/>
+            <Input value={password} onChange={handlePWChange} type='password'/>
           </PasswordContainer>
-          <Button>Sign In</Button>
+          <Button onClick={handleSignin}>Sign In</Button>
         </NormalSignInContainer>
       </SignInContainer>
     </SignInBackground>
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+  authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+  databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
+  projectId: process.env.REACT_APP_FIREBASE_projectId,
+  storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+  appId: process.env.REACT_APP_FIREBASE_appId,
+  measurementId: process.env.REACT_APP_FIREBASE_measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const db = getFirestore(app);
